67e3eba47c233a1770eb098011aa1f89c667f944
Huiyi Victoria Lyu <lyuha@bc.edu>
2020-01-21 10:52:24 -0500

    6 sets of pictures

--- You are Awesome!!!!/ViewController.swift
+++ You are Awesome!!!!/ViewController.swift
@@ -12,30 +12,47 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var UIIMAGEVIEW: UIImageView!
+    
+    var imageNumber=0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func messagebuttonpressed(_ sender: UIButton) {
         
-        let awesomeMessage="You Are Awesome!"
-        let greatMessage = "You Are Great!"
-        let bombMessage = "You Are Da Bomb"
+        print(imageNumber)
+        let imageName = "image" + String(imageNumber)
+        UIIMAGEVIEW.image = UIImage(named: imageName)
+        imageNumber = imageNumber+1
+        if imageNumber == 6 {
+            imageNumber=0
+        }
         
-        if messageLabel.text == awesomeMessage {
-            messageLabel.text = greatMessage
-            UIIMAGEVIEW.image = UIImage(named: "IMG_7743")
-            
-        } else if messageLabel.text==greatMessage  {
-            messageLabel.text = bombMessage
-            UIIMAGEVIEW.image = UIImage(named: "image0")
-        }else {
-            messageLabel.text = awesomeMessage
-            UIIMAGEVIEW.image = UIImage(named: "IMG_3725")
-            
-            
-       
-    }
+        
+        
+     
+//
+//        let awesomeMe ssage="You Are Awesome!"
+//        let greatMessage = "You Are Great!"
+//        let bombMessage = "You Are Da Bomb"
+//
+//        if messageLabel.text == awesomeMessage {
+//            messageLabel.text = greatMessage
+//            UIIMAGEVIEW.image = UIImage(named: "image2")
+//
+//        } else if messageLabel.text==greatMessage  {
+//            messageLabel.text = bombMessage
+//            UIIMAGEVIEW.image = UIImage(named: "image0")
+//        }else {
+//            messageLabel.text = awesomeMessage
+//            UIIMAGEVIEW.image = UIImage(named: "image1")
+//
+//
+//
+//    }
 }
 
 }
